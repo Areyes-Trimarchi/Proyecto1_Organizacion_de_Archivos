651b15bdb708cf581539341bac8f8bc6db9d1811
Areyes-Trimarchi <alvaro.reyes@unitec.edu>
2015-08-12 17:56:56 -0600

    Agregar el .cpp de escribir clientes en archivo binario, solo esta el struct y un main vacio

---
+++ escritura_cliente.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct Cliente{
+	char name[40];
+	char genero;
+	int idCiudad;
+	int idCliente;
+};
+
+int main(int argc, char* argv[]){
+	
+}
