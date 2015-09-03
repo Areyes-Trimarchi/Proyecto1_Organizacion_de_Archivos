db256d9a12603682f767711a00a38d622f7a161b
Dario Villalta <root@Darios-MacBook-Air.local>
2015-09-02 19:44:13 -0600

    Agregando el archivo de ciudades

---
+++ crud_Ciudades.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+
+	int opcionMenuPrincipal;
+	cout << "******* MENU *******\n1) Agregar\n
+	3) Modificar\n
+	4) Listar\n
+	5) Borrar\n
+	6) Buscar\n
+	7) Reindexar" << endl;
+	cin >> opcionMenuPrincipal;
+	switch(opcionMenuPrincipal){
+		case 1:{
+			
+		}
+		break;
+		case 2:{
+
+		}
+		break;
+		case 3:{
+
+		}
+		break;
+		case 4:{
+
+		}
+		break;
+		case 5:{
+
+		}
+		break;
+		case 6:{
+			int opcionBuscar:
+			cout << "\t1)Sin indice\n\t2)con indice\n"
+			swicth(opcionBuscar){
+				case 1:{
+
+				}
+				break;
+				case 2:{
+					
+				}
+				break;
+			}
+		}
+		break;
+	}
+	return 0;
+}
