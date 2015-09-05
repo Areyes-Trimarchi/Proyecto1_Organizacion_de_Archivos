26807acfc13394cf776c2a49a2c0f67bd8b04814
Areyes-Trimarchi <alvaro.reyes@unitec.edu>
2015-09-05 17:36:47 -0600

    Trabajando con el crud_cliente agregar

--- crud_clientes.cpp
+++ crud_clientes.cpp
@@ -1,39 +1,77 @@
 #include "crud_clientes.h"
-//#include "index.h"
+#include "index.h"
 #include <iostream>
 #include <string>
 
 using namespace std;
 
 crud_clientes::crud_clientes(){
-
+	ifstream file_index;
+	file_index.open("indexClientes.bin");
+	if(file_index.fail()){
+		Index();
+	}else{
+		IndiceClien index;
+		while(file_index.read(reinterpret_cast<const char*> (&index.at(i)), sizeof(IndiceClien))){
+			indice_clientes.push_back(index);
+		}
+	}
+	file_index.close();
 }
 
 crud_clientes::~crud_clientes(){
 
 }
 
+bool crud_clientes::Index(){
+	Index indice(10,"clientes.bin");
+	ifstream file_index;
+	file_index.open("indexClientes.bin");
+	IndiceClien index;
+	while(file_index.read(reinterpret_cast<const char*> (&index.at(i)), sizeof(IndiceClien))){
+		indice_clientes.push_back(index);
+	}
+	file_index.close();
+}
+
 bool crud_clientes::Agregar(){
-	cout<<"Agregar Cliente"<<endl;
-	string id_Cliente;
-	string nombre_Cliente;
-	int id_Ciudad;
-	char genero;
-	Cliente client;
-	cout<<"Ingrese el nombre del cliente: ";
-	cin>>nombre_Cliente;
-	cout<<"Ingrese el id del cliente(debe de contener 13 digitos): ";
-	cin>>id_Cliente;
-	while(id_Cliente.size()<13){
+	ifstream file_index;
+	file_index.open("indexClientes.bin");
+	if(file_index.fail()){
+		cerr<<"OCURRIO UN ERROR ABRIENDO EL INDEX DE CLEINTES"<<endl;
+	}else{
+		cout<<"Agregar Cliente"<<endl;
+		char nombre_Cliente[40];
+		char id_Cliente[14];
+		int id_Ciudad;
+		char genero;
+		Cliente client;
+		cout<<"Ingrese el nombre del cliente: ";
+		cin>>nombre_Cliente;
 		cout<<"Ingrese el id del cliente(debe de contener 13 digitos): ";
 		cin>>id_Cliente;
-	}
-	cout<<"Ingrese el genero del cliente(M/F): ";
-	cin>>genero;
+		while(id_Cliente.size()<13||VerificarIndex(char[14])){
+			cout<<"Ingrese el id del cliente(debe de contener 13 digitos): ";
+			cin>>id_Cliente;
+		}
+		cout<<"Ingrese el genero del cliente(M/F): ";
+		cin>>genero;
 
-	client.idCliente=id_Cliente;
-	client.nombre_cliente=nombre_Cliente;
-	client.genero=genero;
+		client.idCliente=id_Cliente;
+		client.nombre_cliente=nombre_Cliente;
+		client.genero=genero;
+		Header head;
+		ifstream file_cliente;
+		file_cliente.open();
+		file_cliente.seekg(0);
+		file_cliente.read(reinterpret_cast<char*>(&head), sizeof(Header));
+		if(head.availList==-1){
+
+		}else{
+			
+		}
+		file_index.close();
+	}
 	return true;
 }
 
@@ -67,4 +105,12 @@ bool crud_clientes::Buscar(){
 
 bool crud_clientes::Reindexar(){
 	return true;
+}
+
+bool VerificarIndex(char idCliente[14]){
+	for (int i = 0; i < indice_clientes.size(); ++i)
+	{
+		/* code */
+	}
+	return false;
 }
--- crud_clientes.h
+++ crud_clientes.h
@@ -3,21 +3,33 @@
 
 #include <string>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 struct Cliente{
-	string idCliente;
-	string nombre_cliente;
+	char idCliente[14];
+	char nombre_cliente[40];
 	int idCiudad;
 	char genero;
 };
 
-class crud_clientes{
+struct Header{
+	int availList;
+	int sizeRegistro;
+};
 
+struct IndiceClien{
+	char id_clie_index[14];
+	int RRN_index;
+};
+
+class crud_clientes{
+	vector<IndiceClien> indice_clientes;
 public:
 	crud_clientes();
 	~crud_clientes();
+	bool Index();
 	bool Agregar();
 	bool Borrar();
 	bool Modificar();
@@ -25,6 +37,7 @@ public:
 	bool BuscarIndex();
 	bool Buscar();
 	bool Reindexar();
+	bool VerificarIndex(char[14]);
 };
 
 #endif
