bb6a4885ddeae8b12735a803bdd18268063d6a95
Dario Villalta <root@Darios-MacBook-Air.local>
2015-09-07 00:40:08 -0600

    CRUD Ciudades terminado

--- crud_Ciudades.cpp
+++ crud_Ciudades.cpp
@@ -1,76 +1,264 @@
+#include "index.h"
 #include <iostream>
 #include <stdlib.h>
 #include <fstream>
 
 using namespace std;
 
-struct Ciudad{
-	char name[40];
-	int idCiudad;
-};
-
 ostream& operator<<(ostream& output, const Ciudad& city){
-	output << city.name << "\t" << city.idCiudad << endl;
+	output << city.idCiudad << "\t" << city.name << endl;
 	return output;  
 }
 istream& operator>>(istream& input, Ciudad& city){
-	 input >> city.numero >> city.inicio;
+	 input >> city.idCiudad >> city.name;
 	 return input;
 }
+/*ostream& operator<<(ostream& output, const Indice& city){
+	output << city.id_ciu_index << "\t" << city.RRN_index << endl;
+	return output;  
+}*/
+
+int elementoBorrado(int);
 
 int main(int argc, char const *argv[]){
 
+	//Index indice("indexCiudades.bin");
+	fstream file("ciudades.bin", fstream::binary);
+	file.open("ciudades.bin");
+	//ofstream fileSalida("ciudades.bin", ofstream::binary);
+	Index indice("indexCiudades.bin");
+
+	Header head;
+	file.seekg(0);
+	file.read(reinterpret_cast<char*>(&head), sizeof(Header));
+	int sizeRegistros = head.sizeRegistro;
+	int availList =  head.availList;
+
 	int opcionMenuPrincipal;
-	cout << "******* MENU *******\n1) Agregar\n
-	3) Modificar\n
-	4) Listar\n
-	5) Borrar\n
-	6) Buscar\n
-	7) Reindexar" << endl;
+	cout << "******* MENU *******\n1) Agregar\n" <<
+	"2) Modificar\n" <<
+	"3) Listar\n" <<
+	"4) Borrar\n" <<
+	"5) Buscar\n" <<
+	"6) Reindexar" << endl;
 	cin >> opcionMenuPrincipal;
+	cout << "availList = " << availList << "\t sizeRegistro = " << sizeRegistros << endl; 
 	switch(opcionMenuPrincipal){
 		case 1:{
-			Ciudad city;
-			ifstream file("data.bin", ifstream::binary);
-			int cont = 0 ;
-			while(file.read(reinterpret_cast<char*>(&city), sizeof(city))){
-				cout << city << endl;
-				cout << cont << endl;
-				cont++;
+			cout << "\tAgregar una Ciudad" << endl;
+
+			int ID;
+			char NOM[40];
+			cout << "Ingrese el id de la ciudad: " << endl;
+			cin >> ID;
+			cout << "Ingrese el nombre de la ciudad: " << endl;
+			cin >> NOM;
+			Ciudad ciudadNueva;
+			ciudadNueva.idCiudad = ID;
+			strcpy(ciudadNueva.name, NOM);
+			indice.add(ciudadNueva, sizeRegistros /*+ elementoBorrado(sizeRegistros)*/);
+			//cout << "Pos 1 = " << file.tellp() << endl;
+			if (availList == -1)
+				file.seekp (0, file.end);
+			else{
+				Ciudad city;
+				//int rrn = indice.ciudadRRN(availList, city).RRN_index;
+				//cout << "Avail = " << availList << "\tRRN = " << rrn << endl;
+				int ecuacion = ( sizeof(Header) + /*( elementoBorrado(rrn) * sizeof(Ciudad) ) + */( sizeof(Ciudad) * availList) );
+				file.seekg(0);
+				file.seekg(ecuacion);
+				file.read(reinterpret_cast<char*>(&city), sizeof(Ciudad));
+				availList = atoi(city.name);
+				head.availList = availList;
+
+				file.seekp(0);
+				file.seekp(ecuacion);
 			}
+			//cout << "Pos 2 = " << file.tellp() << endl;
+			file.write(reinterpret_cast<char*>(&ciudadNueva), sizeof(Ciudad));
+			//cout << "Pos 3 = " << file.tellp() << endl;
+			file.seekp (0, file.beg);
+			//cout << "Pos 4 = " << file.tellp() << endl;
+			sizeRegistros++;
+			head.sizeRegistro = head.sizeRegistro + 1;
+			file.write(reinterpret_cast<char*>(&head), sizeof(Header));
+			//cout << "Pos 5 = " << file.tellp() << endl;
 		}
 		break;
 		case 2:{
+			cout << "\tModificar una Ciudad" << endl;
+
+			int ID;
+			int IDNUEVO;
+			char NOM[40];
+			cout << "Ingrese el id de la ciudad a modificar: " << endl;
+			cin >> ID;
+			cout << "Ingrese el NUEVO id de la ciudad: " << endl;
+			cin >> IDNUEVO;
+			cout << "Ingrese el NUEVO nombre de la ciudad: " << endl;
+			cin >> NOM;
+
+			Ciudad ciudadNueva;
+			ciudadNueva.idCiudad = IDNUEVO;
+			strcpy(ciudadNueva.name, NOM);
+
+			Ciudad ciudadVieja;
+			ciudadVieja.idCiudad = ID;
+			strcpy(ciudadVieja.name, "NOM");
+			Indice ind = indice.get(ciudadVieja);
+			int meter = indice.ciudadRRN(ind.RRN_index, ciudadVieja).RRN_index;
+			indice.remove(ciudadVieja);
+			int rrn = ind.RRN_index;
+			int ecuacion = ( sizeof(Header) + /*( elementoBorrado(rrn) * sizeof(Ciudad) ) + */( sizeof(Ciudad) * rrn) );
+			cout << "meter = " << meter;
+			indice.add(ciudadNueva, meter);
+
+			file.seekp(0);
+			file.seekp(ecuacion);
+			file.write(reinterpret_cast<char*>(&ciudadNueva), sizeof(Ciudad));
 
 		}
 		break;
 		case 3:{
+			cout << "\tListar las Ciudades" << endl << endl;
 
-		}
-		break;
-		case 4:{
-
-		}
-		break;
-		case 5:{
-
-		}
-		break;
-		case 6:{
-			int opcionBuscar:
-			cout << "\t1)Sin indice\n\t2)con indice\n"
-			swicth(opcionBuscar){
+			int opcionListar;
+			cout << "1)Sin Borrados\n2)Con Borrados" << endl << endl;
+			cin >> opcionListar;
+			switch(opcionListar){
 				case 1:{
-
+					Ciudad city;
+					int rrn;
+					cout << "sizeRegistros= " << sizeRegistros << endl;
+					for (int i = 0; i < sizeRegistros ; ++i){
+						rrn = indice.at(i, city).RRN_index;
+						int ecuacion = ( sizeof(Header) + /*( elementoBorrado(rrn) * sizeof(Ciudad) ) + */( sizeof(Ciudad) * rrn) );
+						//cout << "Ecuacion = " << ecuacion << "\tRRN = " << rrn << endl;
+						file.seekg(0);
+						file.seekg(ecuacion);
+						file.read(reinterpret_cast<char*>(&city), sizeof(Ciudad));
+						cout << city;
+					}
 				}
 				break;
 				case 2:{
+					Ciudad city;
+					while(file.read(reinterpret_cast<char*>(&city), sizeof(Ciudad))){
+						cout << city;
+					}
+				}
+				break;
+			}
+			
+		}
+		break;
+		case 4:{
+			cout << "\tEliminar una Ciudad" << endl;
 
+			int ID;
+			cout << "Ingrese el id de la ciudad a eliminar: " << endl;
+			cin >> ID;
+			Ciudad ciudadNueva;
+			ciudadNueva.idCiudad = ID;
+			strcpy(ciudadNueva.name, "NOM");
+			Indice ind = indice.get(ciudadNueva);
+			//availList =  ind.RRN_index;
+			indice.remove(ciudadNueva);
+			int rrn = ind.RRN_index;
+			int ecuacion = ( sizeof(Header) + /*( elementoBorrado(rrn) * sizeof(Ciudad) ) + */( sizeof(Ciudad) * rrn) );
+			file.seekp(0);
+			file.seekp(ecuacion);
+			Ciudad borrada;
+			borrada.idCiudad = -99;
+			string s = to_string(availList);
+			char const *pchar = s.c_str();
+			strcpy(borrada.name, pchar);
+
+			file.write(reinterpret_cast<char*>(&borrada), sizeof(Ciudad));
+			sizeRegistros--;
+			head.sizeRegistro = sizeRegistros - 1;
+			availList =  ind.RRN_index;
+			head.availList = availList;
+			file.seekp (0, file.beg);
+			file.write(reinterpret_cast<char*>(&head), sizeof(Header));
+		}
+		break;
+		case 5:{
+			int opcionBuscar;
+			cout << "1)Sin indice\n2)Con indice" << endl;
+			cin >> opcionBuscar;
+			switch(opcionBuscar){
+				case 1:{
+					int idBuscar;
+					cout << "Ingrese el ID de la Ciudad que desea encontrar: " << endl;
+					cin >> idBuscar;
+					Ciudad city;
+					file.seekg(0, file.beg);
+					file.read(reinterpret_cast<char*>(&head), sizeof(Header));
+					bool encontro = false;
+					while(file.read(reinterpret_cast<char*>(&city), sizeof(city))){
+						if(idBuscar == city.idCiudad){
+							encontro = true;
+							break;
+						}
+					}
+					if(encontro)
+						cout << "Busqueda realizada con exito.\nLa ciudad que busca es: " << city;
+					else
+						cout << "La ciudad no existe en la base de datos." << endl;
+				}
+				break;
+				case 2:{
+					int idBuscar;
+					cout << "Ingrese el ID de la Ciudad que desea encontrar: " << endl;
+					cin >> idBuscar;
+					Ciudad city;
+					city.idCiudad = idBuscar;
+					strcpy(city.name, "NOM");
+					Indice ind = indice.get(city);
+					int rrn = ind.RRN_index;
+					if(rrn != -99){
+						int ecuacion = ( sizeof(Header) + /*( elementoBorrado(rrn) * sizeof(Ciudad) ) + */( sizeof(Ciudad) * rrn) );
+						file.seekg(0);
+						file.seekg(ecuacion);
+						file.read(reinterpret_cast<char*>(&city), sizeof(Ciudad));
+						cout << "Busqueda realizada con exito.\nLa ciudad que busca es: " << city;
+					} else
+						cout << "La ciudad no existe en la base de datos." << endl;
 				}
 				break;
 			}
 		}
 		break;
+		case 6:{
+			cout << "\tReindexar" << endl;
+			indice.create("indexCiudades.bin");
+			cout << "Reindexado con exito." << endl;
+		}
+		break;
+		default:{
+			cout << "Error." << endl;
+		}
+		break;
 	}
+	file.close();
 	return 0;
+}
+
+int elementoBorrado(int RRN){
+	int borrados = 0;
+	int azterizco = -99;
+	//char azterizco[1];
+	//azterizco[0] = '*';
+	Ciudad city;
+	int cont = 0;
+	ifstream file("ciudades.bin", ifstream::binary);
+	file.seekg(8);
+	while(file.read(reinterpret_cast<char*>(&city), sizeof(Ciudad))){
+		if(city.idCiudad == azterizco && cont < RRN)
+			borrados++;
+		cont++;
+	}
+	file.close();
+	return borrados;
 }
